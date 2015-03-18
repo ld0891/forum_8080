69606da1c9539c15ded57b4fe9f1e0ecbb30033f
ld0891 <ld0891@live.com>
2015-03-18 22:58:59 +0800

    Added SloppySwiper.

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -66,36 +66,6 @@
 #define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
 #define COCOAPODS_VERSION_PATCH_MBProgressHUD 1
 
-// MMDrawerController
-#define COCOAPODS_POD_AVAILABLE_MMDrawerController
-#define COCOAPODS_VERSION_MAJOR_MMDrawerController 0
-#define COCOAPODS_VERSION_MINOR_MMDrawerController 5
-#define COCOAPODS_VERSION_PATCH_MMDrawerController 7
-
-// MMDrawerController/Core
-#define COCOAPODS_POD_AVAILABLE_MMDrawerController_Core
-#define COCOAPODS_VERSION_MAJOR_MMDrawerController_Core 0
-#define COCOAPODS_VERSION_MINOR_MMDrawerController_Core 5
-#define COCOAPODS_VERSION_PATCH_MMDrawerController_Core 7
-
-// MMDrawerController/MMDrawerBarButtonItem
-#define COCOAPODS_POD_AVAILABLE_MMDrawerController_MMDrawerBarButtonItem
-#define COCOAPODS_VERSION_MAJOR_MMDrawerController_MMDrawerBarButtonItem 0
-#define COCOAPODS_VERSION_MINOR_MMDrawerController_MMDrawerBarButtonItem 5
-#define COCOAPODS_VERSION_PATCH_MMDrawerController_MMDrawerBarButtonItem 7
-
-// MMDrawerController/MMDrawerVisualStates
-#define COCOAPODS_POD_AVAILABLE_MMDrawerController_MMDrawerVisualStates
-#define COCOAPODS_VERSION_MAJOR_MMDrawerController_MMDrawerVisualStates 0
-#define COCOAPODS_VERSION_MINOR_MMDrawerController_MMDrawerVisualStates 5
-#define COCOAPODS_VERSION_PATCH_MMDrawerController_MMDrawerVisualStates 7
-
-// MMDrawerController/Subclass
-#define COCOAPODS_POD_AVAILABLE_MMDrawerController_Subclass
-#define COCOAPODS_VERSION_MAJOR_MMDrawerController_Subclass 0
-#define COCOAPODS_VERSION_MINOR_MMDrawerController_Subclass 5
-#define COCOAPODS_VERSION_PATCH_MMDrawerController_Subclass 7
-
 // MWPhotoBrowser
 #define COCOAPODS_POD_AVAILABLE_MWPhotoBrowser
 #define COCOAPODS_VERSION_MAJOR_MWPhotoBrowser 1
@@ -131,6 +101,12 @@
 #define COCOAPODS_VERSION_MAJOR_SWRevealViewController 2
 #define COCOAPODS_VERSION_MINOR_SWRevealViewController 3
 #define COCOAPODS_VERSION_PATCH_SWRevealViewController 0
+
+// SloppySwiper
+#define COCOAPODS_POD_AVAILABLE_SloppySwiper
+#define COCOAPODS_VERSION_MAJOR_SloppySwiper 0
+#define COCOAPODS_VERSION_MINOR_SloppySwiper 3
+#define COCOAPODS_VERSION_PATCH_SloppySwiper 0
 
 // hpple
 #define COCOAPODS_POD_AVAILABLE_hpple
